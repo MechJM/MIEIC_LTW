762af4cf79e879398a496fc07ec758e26da3b409
MechJM <matosjoaops@protonmail.com>
2020-10-16 11:00:08 +0100

    Made progress with ex5

--- php/exercise3/list_news.php
+++ php/exercise3/list_news.php
@@ -1,19 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="PT-pt">
     <head>
         <title>SQLite</title>
     </head>
     <body>
         <?php
             $db = new PDO("sqlite:news.db");
-            $stmt = $db->prepare("select * from news");
+            $stmt = $db->prepare("SELECT news.*, users.*, COUNT(comments.id) AS comments
+            FROM news JOIN
+                 users USING (username) LEFT JOIN
+                 comments ON comments.news_id = news.id
+            GROUP BY news.id, users.username
+            ORDER BY published DESC
+            ");
             $stmt->execute();
             $articles = $stmt->fetchAll();
 
-            foreach( $articles as $article) {
-                echo '<h1>' . $article['title'] . '</h1>';
-                echo '<p>' . $article['introduction'] . '</p>';
-              }
+            foreach( $articles as $article)
+            {
+                echo "<article>";
+                echo "<header>";
+                echo "<h1>" . $article["title"] . "</h1>";
+                echo "</header>";
+                echo "<p>" . $article["introduction"] ."</p>";
+                echo "<p>" . $article["fulltext"] . "</p>";
+                echo "<footer>";
+                echo '<span class="author">';
+                echo $article["username"];
+                echo "</span>";
+                echo "</footer>";
+                echo "</article>";
+            }
             
         ?>
     </body>
