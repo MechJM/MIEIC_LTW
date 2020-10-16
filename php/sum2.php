1a69074f3e6b52ee5bc3ed0ff92a92633ee3ca6b
MechJM <matosjoaops@protonmail.com>
2020-10-16 10:00:50 +0100

    Did php exs 1 and 2

---
+++ php/sum2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="PT-pt">
+    <head>
+        <title>
+            Sum
+        </title>
+    </head>
+    <body>
+       <?php
+
+        $num1 = $_GET["num1"];
+        $num2 = $_GET["num2"];
+        $sum = $num1 + $num2;
+
+        echo "Sum: $sum<br>";
+
+        ?> 
+        <a href="form2.html">Form</a>
+    </body>
+</html>
+
