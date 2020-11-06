5e6d917df742f73606010fe6a44b34ec23e2872d
MechJM <matosjoaops@protonmail.com>
2020-11-06 10:56:30 +0000

    Did ex7

--- javascript/products/script.js
+++ javascript/products/script.js
@@ -3,27 +3,23 @@
 function addTableEntry(description, amount)
 {
     let entry = document.createElement("tr");
-    entry.innerHTML = `<tr><td>${description}</td><td><input value="${amount}"</td><td><input type="button" value="Remove"</td></tr>`;
+    entry.innerHTML = `<td>${description}</td><td><input value="${amount}"</td><td><input type="button" value="Remove"></td>`;
     let table = document.getElementById("products");
     table.append(entry);
+
+    let button = entry.querySelector("input[type=button]");
+    console.log(button);
+    button.addEventListener("click",function() {
+        console.log("Button was clicked!");
+        this.parentNode.parentNode.remove();
+    });
 }
 
 let form = document.getElementsByTagName("form")[0];
 form.addEventListener("submit",function(event){
     alert("Submitted!");
-    event.preventDefault();
+    
     addTableEntry(document.querySelector("label:nth-child(1) > input").value,document.querySelector("label:nth-child(2) > input").value);
+    event.preventDefault();
 });
 
-console.log(form.outerHTML);
-
-let secondInput = document.querySelector("label:nth-child(2) > input");
-
-console.log(secondInput.outerHTML);
-
-let inputs = document.querySelectorAll("form input");
-
-for (let input of inputs)
-{
-    console.log(input.outerHTML);
-}
