42827c17865024abcacbd20b4490471141255d57
MechJM <matosjoaops@protonmail.com>
2020-11-06 09:17:01 +0000

    Did ex4

--- javascript/products/script.js
+++ javascript/products/script.js
@@ -1,8 +1,21 @@
 "use strict";
 
-//window.addEventListener("load",function(){
-    let products = document.getElementById("products");
-    console.log(products);
-//});
+let form = document.getElementsByTagName("form");
 
+console.log(form);
 
+for (let formElem of form)
+{
+    console.log(formElem.outerHTML);
+}
+
+let secondInput = document.querySelector("label:nth-child(2) > input");
+
+console.log(secondInput.outerHTML);
+
+let inputs = document.querySelectorAll("form input");
+
+for (let input of inputs)
+{
+    console.log(input.outerHTML);
+}
