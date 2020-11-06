c44cca32b24f6006931be54e5c8668dde793ebdb
MechJM <matosjoaops@protonmail.com>
2020-11-06 09:49:51 +0000

    Did ex6

--- javascript/products/script.js
+++ javascript/products/script.js
@@ -1,9 +1,18 @@
 "use strict";
 
+function addTableEntry(description, amount)
+{
+    let entry = document.createElement("tr");
+    entry.innerHTML = `<tr><td>${description}</td><td><input value="${amount}"</td><td><input type="button" value="Remove"</td></tr>`;
+    let table = document.getElementById("products");
+    table.append(entry);
+}
+
 let form = document.getElementsByTagName("form")[0];
 form.addEventListener("submit",function(event){
     alert("Submitted!");
     event.preventDefault();
+    addTableEntry(document.querySelector("label:nth-child(1) > input").value,document.querySelector("label:nth-child(2) > input").value);
 });
 
 console.log(form.outerHTML);
