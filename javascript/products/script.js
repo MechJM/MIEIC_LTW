9c6c16acfc46751c8fd6a351f792e7907d795855
MechJM <matosjoaops@protonmail.com>
2020-11-19 19:13:58 +0000

    Finished the first section of js exercises

--- javascript/products/script.js
+++ javascript/products/script.js
@@ -1,18 +1,33 @@
 "use strict";
 
+function updateTotal()
+{
+    let total = 0;
+    let inputs = document.querySelectorAll("td:nth-child(2) > input");
+    for (const input of inputs) {
+        total += Number(input.value);
+    }
+    document.getElementById("total").innerHTML = total;
+}
+
 function addTableEntry(description, amount)
 {
     let entry = document.createElement("tr");
     entry.innerHTML = `<td>${description}</td><td><input value="${amount}"</td><td><input type="button" value="Remove"></td>`;
     let table = document.getElementById("products");
     table.append(entry);
+    updateTotal();
 
     let button = entry.querySelector("input[type=button]");
-    console.log(button);
+    //console.log(button);
     button.addEventListener("click",function() {
         console.log("Button was clicked!");
         this.parentNode.parentNode.remove();
+        updateTotal();
     });
+
+    let inputValue = entry.querySelector("td:nth-child(2) > input");
+    inputValue.addEventListener("change",updateTotal);
 }
 
 let form = document.getElementsByTagName("form")[0];
@@ -22,4 +37,3 @@ form.addEventListener("submit",function(event){
     addTableEntry(document.querySelector("label:nth-child(1) > input").value,document.querySelector("label:nth-child(2) > input").value);
     event.preventDefault();
 });
-
