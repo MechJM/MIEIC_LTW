6128ec5032d7fd48d165ffacecac34ee09744adc
MechJM <matosjoaops@protonmail.com>
2021-01-14 12:10:56 +0000

    Finished 2017 exam

--- Exame/2017/part_2_ex3_b.js
+++ Exame/2017/part_2_ex3_b.js
@@ -7,15 +7,24 @@ function encodeForAjax(data)
     }).join('&')
 }
 
-function addMoreImages()
+function addMoreImages(event)
 {
+    let newImages = JSON.parse(this.responseText);
 
+    let list = document.querySelector("div#photos > ul");
+
+    for (let image of newImages)
+    {
+      let newItem = document.createElement("li");
+      newItem.innerHTML = `<img src = ${image}>`;
+      list.appendChild(newItem);
+    }
 }
 
 loadLink.addEventListener("click", function(event) {
     event.preventDefault();
     let request = new XMLHttpRequest();
-    request.onload = addMoreImages;
+    request.addEventListener("load", addMoreImages);
     request.open("get", "getrandomimages.php", true);
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     request.send(encodeForAjax({}));
